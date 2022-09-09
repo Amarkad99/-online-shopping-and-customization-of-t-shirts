ed72af9ce157755df242702ba42dcfe61d2e9bbb
Abhijit_Thange <thangeabhijit1998@gmail.com>
2022-09-09 15:02:35 +0530

    abhijit_React_Component

--- treandy_t/src/App.js
+++ treandy_t/src/App.js
@@ -1,9 +1,29 @@
 
+import Profile from "./Component/ProfilePage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Button from 'react-bootstrap/Button';
+//import { Button } from 'react-bootstrap';
+//import '../scss/custom.scss';
 
+import CreateDesign from "./Component/CreateDesign";
+import MyOrder from "./Component/OrderDetails";
+import Redymade from "./Component/Redymade";
+import DeliveryDetails from "./Component/DeliveryDetails";
 
 function App() {
   return (
-   <div> this is our first pproject</div>
+   <div>
+    {/*
+    <Profile></Profile>
+    <CreateDesign></CreateDesign>
+    <MyOrder></MyOrder>
+     <Redymade></Redymade>
+  */}
+    <CreateDesign></CreateDesign>
+    <DeliveryDetails></DeliveryDetails>
+     
+   </div>
+
    
   );
 }
