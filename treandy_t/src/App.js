23764fad0273fe947504618b63388ecfcd983ce1
Pavanjadhav1 <pavanjadhav196@gmail.com>
2022-09-09 14:44:04 +0530

    React components

--- treandy_t/src/App.js
+++ treandy_t/src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import Tshirt_Details from "./Tshirt_Details";
+// import Orderhistory from "./Orderhistory";
+// import Create from "./CreateDesign";
+// import ShippingDetails from "./ShippingDetails";
+// import Report from "./Report";
 
+// import Payment from "./Payment";
 
 
 function App() {
   return (
-   <div> this is our first pproject</div>
-   
-  );
+  //  <Payment>  </Payment>
+//  <Report>  </Report>
+  //  <ShippingDetails>  </ShippingDetails>
+//  <Create>  </Create>
+ //<Orderhistory>  </Orderhistory> 
+  <Tshirt_Details></Tshirt_Details>
+
+  )
 }
 
 export default App;
