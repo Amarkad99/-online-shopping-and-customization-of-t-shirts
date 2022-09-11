618a1646ef1610a03b74f5d67d64fc3ab1ce791b
Abhijit_Thange <thangeabhijit1998@gmail.com>
2022-09-11 15:26:54 +0530

    Auto stash before merge of "developer" and "origin/developer"

--- treandy_t/src/Component/ProfilePage.js
+++ treandy_t/src/Component/ProfilePage.js
@@ -1,19 +1,50 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+
 import box from "../CssClass/Box.css";
 import Button from 'react-bootstrap/Button';
 import Logo from './logo.png';
 import NavBar from "./NavBar";
-//import CreateDesign from "./Component/CreateDesign";
+
 
 export default function Profile()
-{
+{ 
+    let [id,setId]=useState("")
     let [fname,setFname]=useState("")
     let [lname,setLname]=useState("")
     let [gender,setGender]=useState("")
     let [email,setEmail]=useState("")
     let [mobileno,setMobileNo]=useState("")
     let [address,setAddress]=useState("")
+    let [street,setStreet]=useState("")
+    let [city,setCity]=useState("")
+    let [pincode,setPincode]=useState("")
 
+
+    const params=useParams();
+
+    useEffect(()=>{
+        
+        getUserDetails();
+    },[])
+
+    const getUserDetails=async()=>{
+        console.warn("param=",params)
+        let result=await fetch(`http://localhost:8080/user/profile/1`);
+        result = await result.json();
+       console.warn("Result=",result);
+
+       setFname(result.first_name);
+       setMobileNo(result.mobile_number);
+       setId(result.user_id);
+        
+    }
+    
+
+    function Id(event)
+    {
+       setId(event.target.value)
+    }
      function FirstName(event)
      {
         setFname(event.target.value)
@@ -72,51 +103,54 @@ export default function Profile()
         <center><h1>Profile</h1></center> 
         <form method="" action="">
     <div class="form-group">
+        <label for="User Id">User Id:</label>
+        <input type="text" defaultValue={id} class="form-control" id="id" name="id" onBlur={Id} />
+    </div>
+    <div class="form-group">
         <label for="First Name">First Name:</label>
-        <input type="text" class="form-control" id="firstName" name="firstName" onBlur={FirstName} />
+        <input type="text" defaultValue={fname} class="form-control" id="firstName" name="firstName" onBlur={FirstName} />
     </div>
     <div class="form-group">
         <label for="Last Name">Last Name:</label>
-        <input type="text" class="form-control" id="lastName" name="lastName" onBlur={LastName}  />
+        <input type="text" defaultValue={lname} class="form-control" id="lastName" name="lastName" onBlur={LastName}  />
     </div>
     <div class="form-group">
         <label for="Gender">Gender:</label>
-        <input type="text" class="form-control" id="gender" name="gender" onBlur={Gender} />
+        <input type="text" defaultValue={gender} class="form-control" id="gender" name="gender" onBlur={Gender} />
     </div>
     <div class="form-group">
         <label for="Email Id">Email:</label>
-        <input type="email" class="form-control" id="emailId" name="emailId" onBlur={EmailId} />
+        <input type="email" defaultValue={email} class="form-control" id="emailId" name="emailId" onBlur={EmailId} />
     </div>
     <div class="form-group">
         <label for="Mobile No">Mobile No:</label>
-        <input type="text" class="form-control" id="mobileNo" name="mobileNo"  onBlur={MobileNo}/>
+        <input type="text" defaultValue={mobileno} class="form-control" id="mobileNo" name="mobileNo"  onBlur={MobileNo}/>
     </div>
     <div class="form-group">
         <label for="Address">Address Line:</label>
-        <input type="text" class="form-control" id="address" name="address" onBlur={Address}/>
+        <input type="text" defaultValue={address} class="form-control" id="address" name="address" onBlur={Address}/>
     </div>
     <div class="form-group">
         <label for="Street">Street:</label>
-        <input type="text" class="form-control" id="street" name="city" onBlur={Street}/>
+        <input type="text" defaultValue={street} class="form-control" id="street" name="city" onBlur={Street}/>
     </div>
     <div class="form-group">
         <label for="City">City:</label>
-        <input type="text" class="form-control" id="city" name="city" onBlur={City}/>
+        <input type="text" defaultValue={city} class="form-control" id="city" name="city" onBlur={City}/>
     </div>
     <div class="form-group">
         <label for="Pin Code">Pin Code:</label>
-        <input type="text" class="form-control" id="pinCode" name="pinCode" onBlur={PinCode}/>
+        <input type="text" defaultValue={pincode} class="form-control" id="pinCode" name="pinCode" onBlur={PinCode}/>
     </div>
 
     <Button variant="flat" size="xl" onClick={Update}>
         Update
       </Button> 
+
+      
   
     </form>
      </div>
         
     )
 }
-
-//<button type="submit" class="btn btn-default" onClick={profile}>Submit</button>
-//<button  type="submit" class="btn btn-default" onClick={update}>Edit</button>
